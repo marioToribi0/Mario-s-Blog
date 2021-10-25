204b4db23cc65ea3cd739439e16a342bfb6b0992
Mario Toribio <yuniormtr@gmail.com>
2021-10-25 00:56:32 -0400

    Version final

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ class Comment(db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
     text = db.Column(db.Text, nullable=False)
 
-db.create_all()
+#db.create_all()
 
 
 @app.route('/')
